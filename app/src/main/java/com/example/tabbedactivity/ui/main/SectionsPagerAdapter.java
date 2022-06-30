29658ed9c484c371f9467e8407849ed7f5614b96
jjungnii <jungeun0831@postech.ac.kr>
2022-06-30 16:03:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tabbedactivity/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.tabbedactivity.ui.main;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.tabbedactivity.R;
+import com.example.tabbedactivity.Tab1;
+import com.example.tabbedactivity.Tab2;
+import com.example.tabbedactivity.Tab3;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new Tab1();
+                break;
+            case 1:
+                fragment = new Tab2();
+                break;
+            case 2:
+                fragment = new Tab3();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
