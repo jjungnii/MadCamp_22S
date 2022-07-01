9882ae9c48d7702ee34f486ed68c05debeeb871f
jjungnii <jungeun0831@postech.ac.kr>
2022-07-01 11:29:01 +0900

    Tab2: zoom selected image

--- app/src/main/java/com/example/tabbedactivity/FullScreenActivity.java
+++ app/src/main/java/com/example/tabbedactivity/FullScreenActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+
+import com.ablanco.zoomy.Zoomy;
 
 public class FullScreenActivity extends AppCompatActivity {
 
@@ -26,5 +29,12 @@ public class FullScreenActivity extends AppCompatActivity {
         ImageAdapter imageAdapter = new ImageAdapter(this);
 
         imageView.setImageResource(imageAdapter.imageArray[position]);
+
+        Zoomy.Builder builder=new Zoomy.Builder(this)
+                .target(imageView)
+                .animateZooming(false)
+                .enableImmersiveMode(false);
+
+        builder.register();
     }
 }
